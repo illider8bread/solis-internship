5c101d2eb584cc1ff84ba2a3681c3029fce32a53
illider8bread <202684742+illider8bread@users.noreply.github.com>
2025-09-26 19:58:13 -0600

    Corrected styling on carousel navigation arrows

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -4,7 +4,47 @@ import Slider from "react-slick";
 
 const HotCollections = (collections) => {
   let collectionsData = collections.collections
-  let sliderRef= useRef(null);
+  let sliderRef = useRef(null);
+
+
+  function NextArrow({ onClick }) {
+    return (
+      <div
+        onClick={onClick}
+        className="custom-arrow next"
+        style={arrowStyle("right")}
+      >
+        <i className="fa fa-chevron-right" style={{ fontSize: 16 }} />
+      </div>
+    );
+  }
+  function PrevArrow({ onClick }) {
+    return (
+      <div
+        onClick={onClick}
+        className="custom-arrow prev"
+        style={arrowStyle("left")}
+      >
+        <i className="fa fa-chevron-left" style={{ fontSize: 16 }} />
+      </div>
+    );
+  }
+  const arrowStyle = (side) => ({
+    position: "absolute",
+    top: "50%",
+    [side]: "-25px",
+    transform: "translateY(-50%)",
+    zIndex: 2,
+    width: 40,
+    height: 40,
+    background: "#fff",
+    borderRadius: "50%",
+    boxShadow: "0 2px 6px rgba(0,0,0,0.2)",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    cursor: "pointer",
+  });
 
   const settings = {
     dots: false,
@@ -12,12 +52,13 @@ const HotCollections = (collections) => {
     speed: 1000,
     slidesToShow: 4,
     slidesToScroll: 1,
-
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
     responsive: [
       {
         breakpoint: 1200,
         settings: {
-          slidesToShow:2,
+          slidesToShow: 2,
         }
       },
       {
@@ -28,7 +69,7 @@ const HotCollections = (collections) => {
       },
       {
         breakpoint: 767,
-        settings:{
+        settings: {
           slidesToShow: 1
         }
       }
@@ -47,8 +88,8 @@ const HotCollections = (collections) => {
             </div>
           </div>
 
-          <Slider ref={slider => {sliderRef=slider}} {...settings}>
-          {collectionsData.map(({id, title, authorImage, nftImage, nftId, authorId, code}) => (
+          <Slider ref={slider => { sliderRef = slider }} {...settings}>
+            {collectionsData.map(({ id, title, authorImage, nftImage, nftId, authorId, code }) => (
               <div className="nft_coll" key={id}>
                 <div className="nft_wrap">
                   <Link to="/item-details">
@@ -68,7 +109,7 @@ const HotCollections = (collections) => {
                   <span>ERC-{code}</span>
                 </div>
               </div>
-          ))}
+            ))}
           </Slider>
         </div>
       </div>
